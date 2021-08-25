feedaa16a1c30f033550a7157bdfa394eda8d7cd
guisouco <guilherme.souzacolacio@gmail.com>
2021-08-25 19:47:39 -0300

    Atributo complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=19):
+    def __init__(self, *filhos, nome=None, idade=19):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Zeze di Camargo')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Guilherme'
-    print(p.nome)
-    print(p.idade)
+    guilherme = Pessoa(nome='Guilherme')
+    zeze = Pessoa(guilherme, nome='Zeze')
+    print(Pessoa.cumprimentar(zeze))
+    print(id(zeze))
+    print(zeze.cumprimentar())
+    print(zeze.nome)
+    print(zeze.idade)
+    for filhos in zeze.filhos:
+        print(filhos.nome)
 
